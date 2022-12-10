b488f2104c66f346a8533cff77c567e703bbe5ba
Fuzes Marcell <fuzes.marcell@gmail.com>
2022-12-10 17:30:17 +0100

    day_10.cpp part 1 done

---
+++ 2022/day_10.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <assert.h>
+
+#include <aoc.h>
+
+uint64_t signal_strength(int64_t cycle, int64_t x) {
+	static int64_t c = 20;
+	if (cycle != c) {
+		return 0;
+	}
+	c += 40;
+
+	return cycle * x;
+}
+
+int main(int argc, char const *argv[])
+{
+	std::string content = read_entire_file("input.txt");
+
+	uint64_t result = 0;
+	int64_t num_cycle = 0;
+	int64_t x = 1;
+	for (const auto line : split(content, "\n")) {
+		char buffer[256];
+		int res = sscanf(std::string(line).c_str(), "%s", buffer);
+		assert(res == 1);
+
+		std::string instruction = buffer;
+		if (instruction == "addx") {
+			int param;
+			int res = sscanf(std::string(line).c_str(), "%s %d", buffer, &param);
+			assert(res == 2);
+
+			++num_cycle;
+			result += signal_strength(num_cycle, x);
+			++num_cycle;
+			result += signal_strength(num_cycle, x);
+			x += param;
+		}
+		else { // noop
+			assert(instruction == "noop");
+			++num_cycle;
+			result += signal_strength(num_cycle, x);
+		}
+	}
+
+	std::cout << "Part 01: " << result << "\n";
+
+	return 0;
+}
