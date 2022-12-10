2dc0df9c22e21f99389879f2af4247a7a497fab5
Fuzes Marcell <fuzes.marcell@gmail.com>
2022-12-10 17:52:59 +0100

    day_10.cpp part 2 done
    Signed-off-by: Fuzes Marcell <fuzes.marcell@gmail.com>

--- 2022/day_10.cpp
+++ 2022/day_10.cpp
@@ -13,9 +13,23 @@ uint64_t signal_strength(int64_t cycle, int64_t x) {
 	return cycle * x;
 }
 
+#define CRT_WIDTH (40)
+#define CRT_HEIGHT (6)
+
+void draw(uint32_t* pixels, int64_t cycle, int64_t x) {
+	--cycle;
+	int64_t px = (cycle % 40);
+	int64_t py = cycle / 40;
+	if ((px == x) || (px == (x - 1)) || (px == (x + 1))) {
+		pixels[py * CRT_WIDTH + px] = 0xffffffff;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	std::string content = read_entire_file("input.txt");
+
+	uint32_t pixels[CRT_HEIGHT * CRT_WIDTH] = {};
 
 	uint64_t result = 0;
 	int64_t num_cycle = 0;
@@ -33,18 +47,42 @@ int main(int argc, char const *argv[])
 
 			++num_cycle;
 			result += signal_strength(num_cycle, x);
+
+			// draw
+			draw(pixels, num_cycle, x);
+
 			++num_cycle;
 			result += signal_strength(num_cycle, x);
+
+			// draw
+			draw(pixels, num_cycle, x);
+
 			x += param;
 		}
 		else { // noop
 			assert(instruction == "noop");
 			++num_cycle;
 			result += signal_strength(num_cycle, x);
+
+			// draw
+			draw(pixels, num_cycle, x);
 		}
 	}
 
 	std::cout << "Part 01: " << result << "\n";
+
+	for (int y = 0; y < CRT_HEIGHT; y++) {
+		for (int x = 0; x < CRT_WIDTH; x++) {
+			uint32_t v = pixels[y * CRT_WIDTH + x];
+			if (v) {
+				printf("#");
+			}
+			else {
+				printf(".");
+			}
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
