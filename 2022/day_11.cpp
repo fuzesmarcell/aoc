8ee5ce8059e2531ae0d60da6dbe76e6074b582af
Fuzes Marcell <fuzes.marcell@gmail.com>
2022-12-11 16:28:03 +0100

    day_11.cpp finally part 2 done
    Signed-off-by: Fuzes Marcell <fuzes.marcell@gmail.com>

--- 2022/day_11.cpp
+++ 2022/day_11.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <regex>
+#include <assert.h>
 
 #include <aoc.h>
 
@@ -17,9 +18,15 @@ uint64_t old_add_param(uint64_t old, uint64_t param) {
 	return old + param;
 }
 
+enum {
+	MUL,
+	ADD,
+};
+
 struct Monkey {
 	uint64_t inspected;
 	std::vector<uint64_t> items;
+	int expr_type;
 	MonkeyFunc* worry_func;
 	uint64_t param;
 	uint64_t test;
@@ -27,8 +34,60 @@ struct Monkey {
 	uint64_t false_idx;
 };
 
+struct CongruentStatement {
+	uint64_t b;
+	uint64_t mod;
+};
+
+// https://math.stackexchange.com/questions/2442407/find-number-in-a-certain-range-satisfying-three-modulus-requirements
+// https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+uint64_t chinese_remainder_theorem(const std::vector<CongruentStatement>& statements) {
+	uint64_t result = statements.front().b;
+	uint64_t adder = statements.front().mod;
+	for (size_t i = 1; i < statements.size(); i++) {
+		CongruentStatement s = statements[i];
+
+		while ((result % s.mod) != s.b) {
+			result += adder;
+		}
+
+		adder *= s.mod;
+	}
+
+	return result;
+}
+
 int main(int argc, char const *argv[])
 {
+	{
+		std::vector<CongruentStatement> statements;
+		statements.push_back({ 20, 23 });
+		statements.push_back({  8, 19 });
+		statements.push_back({  6, 13 });
+		statements.push_back({ 14, 17 });
+
+		assert(chinese_remainder_theorem(statements) == 74678);
+	}
+
+	{
+		std::vector<CongruentStatement> statements;
+		statements.push_back({ 0, 3 });
+		statements.push_back({ 3, 4 });
+		statements.push_back({ 4, 5 });
+
+		assert(chinese_remainder_theorem(statements) == 39);
+	}
+
+	{
+		std::vector<CongruentStatement> statements;
+		statements.push_back({ 1,  2 });
+		statements.push_back({ 2,  5 });
+		statements.push_back({ 4, 11 });
+
+		assert(chinese_remainder_theorem(statements) == 37);
+	}
+	
+	
 	using std::string_view;
 	using StrArr = std::vector<std::string_view>;
 
@@ -40,6 +99,7 @@ int main(int argc, char const *argv[])
 	std::vector<string_view> lines = split(content, "\n");
 
 	std::vector<Monkey> monkeys;
+	std::vector<uint64_t> divisors;
 
 	for (size_t i = 0; i < lines.size(); ) {
 		StrArr starting_items = split(lines[i + 1], " ");
@@ -53,28 +113,35 @@ int main(int argc, char const *argv[])
 
 		MonkeyFunc* mf = nullptr;
 
+		int e = MUL;
 		int p = 0;
 		if (param != "old") {
 			p = to_int(param);
 
 			if (op == "+") {
+				e = ADD;
 				mf = old_add_param;
 			}
 			else {
+				e = MUL;
 				mf = old_mul_param;
 			}
 		}
 		else {
+			e = MUL;
 			mf = old_mul_old;
 		}
 
 		Monkey m = {
+			.expr_type = e,
 			.worry_func = mf,
 			.param = (uint64_t)p,
 			.test = (uint64_t)to_int(test[test.size() - 1]),
 			.true_idx = (uint64_t)to_int(true_str[true_str.size() - 1]),
 			.false_idx = (uint64_t)to_int(false_str[false_str.size() - 1]),
 		};
+
+		divisors.push_back(m.test);
 
 		std::regex r("\\d+");
 		std::smatch match;
@@ -92,16 +159,54 @@ int main(int argc, char const *argv[])
 	//
 	// logic
 	//
-	for (size_t i = 0; i < 20; i++) {
+	for (size_t i = 0; i < 10000; i++) {
 		for (auto& monkey : monkeys) {
 			for (auto item : monkey.items) {
 				++monkey.inspected;
 				uint64_t worry_level = monkey.worry_func(item, monkey.param);
-				worry_level /= 3;
-				if (worry_level % monkey.test == 0) {
+				//worry_level /= 3;
+
+				std::vector<CongruentStatement> statements;
+				for (auto divisor : divisors) {
+					statements.push_back({
+						.b = worry_level % divisor,
+						.mod = divisor
+						});
+				}
+
+				worry_level = chinese_remainder_theorem(statements);
+
+			#if 0
+				std::vector<uint64_t> remainders;
+				for (auto divisor : divisors) {
+					remainders.push_back(worry_level % divisor);
+				}
+
+				for (size_t j = 0; j < worry_level; j++) {
+					bool match = true;
+					for (size_t k = 0; k < divisors.size(); k++) {
+						if (j % divisors[k] != remainders[k]) {
+							match = false;
+							break;
+						}
+					}
+
+					if (match) {
+						worry_level = j;
+						break;
+					}
+				}
+			#endif // 0
+
+				//assert(new_worry_level == worry_level);
+
+				uint64_t remainder = worry_level % monkey.test;
+				if (remainder == 0) {
+					//std::cout << "Throws true " << worry_level << " % " << monkey.test << " == " << remainder << "\n";
 					monkeys[monkey.true_idx].items.push_back(worry_level);
 				}
 				else {
+					//std::cout << "Throws false " << worry_level << " % " << monkey.test << " == " << remainder << "\n";
 					monkeys[monkey.false_idx].items.push_back(worry_level);
 				}
 			}
@@ -118,6 +223,17 @@ int main(int argc, char const *argv[])
 			std::cout << "\n";
 		}
 		std::cout << "\n...\n";
+	#endif // 0
+
+	#if 1
+		if ((i + 1) % 1000 == 0) {
+			std::cout << "== After round " << i + 1 << " ==\n";
+			int j = 0;
+			for (const auto& monkey : monkeys) {
+				std::cout << "Monkey " << j++ << " inspected items " << monkey.inspected << " times\n";
+			}
+			std::cout << "\n...\n";
+		}
 	#endif // 0
 
 	}
