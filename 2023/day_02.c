e7b999f99f97c9670a4aee03ff9f471dcb71958c
Fuzes Marcell <fuzes.marcell@gmail.com>
2023-12-02 08:06:50 +0100

    2023 day 02 part 2

--- 2023/day_02.c
+++ 2023/day_02.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <limits.h>
 #include <stdbool.h>
+#include <assert.h>
 
 int main() {
 	int n;
@@ -10,33 +11,29 @@ int main() {
 	size_t result = 0;
 	while (scanf("%s %d:", buffer, &n) == 2) {
 		int id = n;
-		int r = 0, g = 0, b = 0;
-		bool possible = true;
+		int r = INT_MIN, g = INT_MIN, b = INT_MIN;
 		while (scanf("%d %s", &n, buffer) == 2) {
 			char c = buffer[strlen(buffer) - 1];
 
 			if (buffer[0] == 'r') {
-				r = n;
+				if (n > r)
+					r = n;
 			}
 			else if (buffer[0] == 'g') {
-				g = n;
+				if (n > g)
+					g = n;
 			}
 			else if (buffer[0] == 'b') {
-				b = n;
-			}
-
-			if (c != ',') {
-				if (r > 12 || g > 13 || b > 14)
-					possible = false;
-				r = 0, g = 0, b = 0;
+				if (n > b)
+					b = n;
 			}
 
 			if (c != ';' && c != ',')
 				break;
 		}
 
-		if (possible)
-			result += id;
+		size_t power = (size_t)r * (size_t)g * (size_t)b;
+		result += power;
 	}
 
 	printf("%zu\n", result);
