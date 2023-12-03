3f6ebbfa2f550c7d6690880221871ad246e8c858
Fuzes Marcell <fuzes.marcell@gmail.com>
2023-12-03 21:47:27 +0100

    Move towards A* or Dijkstra's shortest path algo

--- 2022/day_12.c
+++ 2022/day_12.c
@@ -9,13 +9,13 @@
 static int nx, ny;
 static char map[MAX_DIM][MAX_DIM];
 
+static bool vmap[MAX_DIM][MAX_DIM]; // visited nodes map
+
 static int start_x, start_y;
 static int end_x, end_y;
 
 static inline char get_value(int x, int y);
-static inline void draw_map(int px, int py);
-
-static int traverse_map(int x, int y);
+static int traverse_map(int x, int y, char prev_c, int depth);
 
 int main() {
 	char buffer[512];
@@ -34,7 +34,7 @@ int main() {
 			else if (c == 'E') {
 				end_x = nx;
 				end_y = ny;
-				map[ny][nx] = 'z';
+				map[ny][nx] = 'd'; // TODO: Switch back to 'z'
 			}
 			else {
 				map[ny][nx] = c;
@@ -43,45 +43,9 @@ int main() {
 		++ny;
 	}
 
-#if 0
 	int px = end_x, py = end_y;
-	int prev_x = px, prev_y = py;
 
-	size_t steps = 0;
-	while (!(px == start_x && py == start_y)) {
-
-		draw_map(px, py);
-
-		char c = get_value(px, py);
-		int offsets[4][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-
-		int next_x = -1, next_y = -1;
-		char min = 127;
-		for (int i = 0; i < 4; i++) {
-			int ox = offsets[i][0];
-			int oy = offsets[i][1];
-			char v = get_value(px+ox, py+oy);
-
-			if (px+ox == prev_x && py+oy == prev_y)
-				continue;
-
-			bool is_valid = (v == c || v == c-1);
-			if (is_valid && v < min) {
-				min = v;
-				next_x = px+ox;
-				next_y = py+oy;
-			}
-		}
-
-		assert(c == min || c-1 == min);
-
-		++steps;
-		prev_x = px;
-		prev_y = py;
-		px = next_x;
-		py = next_y;
-	}
-#endif // 0
+	traverse_map(px, py, 'd', 0);
 
 	return 0;
 }
@@ -92,39 +56,36 @@ char get_value(int x, int y) {
 	return map[y][x];
 }
 
-void draw_map(int px, int py) {
-	for (int y = 0; y < ny; y++) {
-		for (int x = 0; x < nx; x++) {
-			if (x == px && y == py) {
-				printf("@");
-			}
-			else {
-				printf("%c", map[y][x]);
-			}
-		}
-		printf("\n");
-	}
-	printf("\n\n");
-}
+int traverse_map(int x, int y, char prev_c, int depth) {
+	if (x == start_x && y == start_y)
+		return depth;
 
-int traverse_map(int x, int y, char prev_c) {
 	char c = get_value(x, y);
-	if (c != prev_c || c == prev_c - 1)
-		return 0;
-
-	int steps[4];
-
-	steps[0] = traverse_map(x - 1, y, c);
-	steps[1] = traverse_map(x + 1, y, c);
-	steps[2] = traverse_map(x, y + 1, c);
-	steps[3] = traverse_map(x, y - 1, c);
-
-	int min = INT_MAX;
-	for (int i = 0; i < 4; i++) {
-		if (steps[i] < min) {
-			min = steps[i];
-		}
+	if (c != prev_c && c != prev_c - 1) {
+		return INT_MAX;
 	}
 
-	return min;
+	// TODO: Instead of storing a normal map of only chars
+	// we need to store the step numbers inside of them
+
+	vmap[y][x] = true;
+
+	int offsets[4][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+	int steps[4] = { INT_MAX, INT_MAX, INT_MAX, INT_MAX };
+	for (int i = 0; i < 4; i++) {
+		int xo = offsets[i][0];
+		int yo = offsets[i][1];
+
+		int px = x + xo;
+		int py = y + yo;
+		if (px < 0 || px >= nx || py < 0 || py >= ny)
+			continue;
+
+		if (!vmap[py][px])
+			steps[i] = traverse_map(px, py, c, depth+1);
+	}
+
+	vmap[y][x] = false;
+
+	return depth-1;
 }
